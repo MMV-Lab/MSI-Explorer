80e2adaa4ddea13fe2e8bfc6fc3f51ffa17f8383
lennartkowitz <91742415+lennartkowitz@users.noreply.github.com>
2023-02-08 16:53:28 +0100

    Update __init__.py

--- src/napari_placeholder/__init__.py
+++ src/napari_placeholder/__init__.py
@@ -1,18 +1,18 @@
 __version__ = "0.0.1"
 
+from ._maldi_ms_data import Maldi_MS
 from ._reader import napari_get_reader
 from ._widget import ExampleQWidget
 from ._writer import write_multiple, write_single_image
 from ._selection import SelectionWindow
 from ._metadata import MetadataWindow
-from ._maldi_ms_data import Maldi_MS
 
 __all__ = (
+    "Maldi_MS",
     "napari_get_reader",
     "write_single_image",
     "write_multiple",
     "ExampleQWidget",
     "SelectionWindow",
     "MetadataWindow",
-    "Maldi_MS",
 )
