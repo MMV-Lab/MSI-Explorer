12b9c8522c3024cdef73debfc18447bfb0cd1900
lennartkowitz <lennart.kowitz@isas.de>
2023-02-10 11:41:30 +0100

    created skeleton for metadata writer function

--- src/napari_placeholder/__init__.py
+++ src/napari_placeholder/__init__.py
@@ -3,15 +3,16 @@ __version__ = "0.0.1"
 from ._maldi_ms_data import Maldi_MS
 from ._reader import napari_get_reader
 from ._widget import ExampleQWidget
-from ._writer import write_multiple, write_single_image
+from ._writer import write_metadata #write_multiple, write_single_image
 from ._selection import SelectionWindow
 from ._metadata import MetadataWindow
 
 __all__ = (
     "Maldi_MS",
     "napari_get_reader",
-    "write_single_image",
-    "write_multiple",
+    "write_metadata",
+    #"write_single_image",
+    #"write_multiple",
     "ExampleQWidget",
     "SelectionWindow",
     "MetadataWindow",
--- src/napari_placeholder/_writer.py
+++ src/napari_placeholder/_writer.py
@@ -1,14 +1,20 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, List, Sequence, Tuple, Union
+#from typing import TYPE_CHECKING, Any, List, Sequence, Tuple, Union
+import csv
 
-if TYPE_CHECKING:
+"""if TYPE_CHECKING:
     DataType = Union[Any, Sequence[Any]]
-    FullLayerData = Tuple[DataType, dict, str]
+    FullLayerData = Tuple[DataType, dict, str]"""
 
 
-def write_single_image(path: str, data: Any, meta: dict) -> List[str]:
-    """Writes a single image layer"""
+def write_metadata(path, data):
+    file = open(path, 'w', newline = '')
+    writer = csv.writer(file)
+    pass
+
+"""def write_single_image(path: str, data: Any, meta: dict) -> List[str]:
+    ""Writes a single image layer""
 
     # implement your writer logic here ...
 
@@ -17,9 +23,9 @@ def write_single_image(path: str, data: Any, meta: dict) -> List[str]:
 
 
 def write_multiple(path: str, data: List[FullLayerData]) -> List[str]:
-    """Writes multiple layers of different types."""
+    ""Writes multiple layers of different types.""
 
     # implement your writer logic here ...
 
     # return path to any file(s) that were successfully written
-    return [path]
+    return [path]"""
