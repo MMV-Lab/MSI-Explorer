8a61e60d6a04b492cc3d321f82dfa91645888440
lampen <lampen@isas.de>
2023-02-23 15:23:03 +0100

    New function get_metadata

--- src/napari_placeholder/_maldi_ms_data.py
+++ src/napari_placeholder/_maldi_ms_data.py
@@ -47,7 +47,7 @@ class Maldi_MS():
         i = self.check_i(i)
         return self._coordinates[i]
 
-    def get_metadata(self):
+    def get_metadata_json(self):
         return json.dumps(self._metadata, sort_keys=False, indent=4)
 
     def get_num_spectra(self):
@@ -68,7 +68,7 @@ class Maldi_MS():
         plt.title(title1)
         plt.show()
 
-    def print_metadata(self):
+    def print_metadata_json(self):
         print(self.get_metadata())
 
     # def print_sf_metadata(self):
@@ -123,3 +123,88 @@ class Maldi_MS():
                 break
 
         return [x3, y3]
+
+    def get_metadata(self):
+        meta = self._metadata
+        d = dict()
+
+        try:
+            d['name'] = \
+                meta['file_description']['source_files']['sf1']['name']
+        except BaseException:
+            pass                                # This path don't exist
+
+        try:
+            d['filter string'] = \
+                meta['referenceable_param_groups']['scan1']['filter string']
+        except BaseException:
+            pass
+
+        try:
+            d['noise level'] = \
+                meta['referenceable_param_groups']['spectrum1']['noise level']
+        except BaseException:
+            pass
+
+        try:
+            sample_keys = meta['samples'].keys()    # object of class dict_keys
+            sample_keys = list(sample_keys)         # e.g. ['essen_kidney']
+            if len(sample_keys) == 1:
+                d['samples'] = sample_keys[0]
+            else:
+                d['samples'] = sample_keys
+        except BaseException:
+            pass
+
+        try:
+            d['max count x'] = \
+                meta['scan_settings']['scansettings1']['max count of pixels x']
+        except BaseException:
+            pass
+
+        try:
+            d['max count y'] = \
+                meta['scan_settings']['scansettings1']['max count of pixels y']
+        except BaseException:
+            pass
+
+        try:
+            d['pixel size x'] = \
+                meta['scan_settings']['scansettings1']['pixel size (x)']
+        except BaseException:
+            pass
+
+        try:
+            d['pixel size y'] = \
+                meta['scan_settings']['scansettings1']['pixel size y']
+        except BaseException:
+            pass
+
+        try:
+            inst_id = meta['instrument_configurations'].keys()
+            inst_id = list(inst_id)
+            inst_id = inst_id[0]                # e.g. 'Q Exactive HF'
+            d['wavelength'] = \
+                meta['instrument_configurations'][inst_id]['components'][0]['wavelength']
+        except BaseException:
+            pass
+
+        try:
+            inst_id = meta['instrument_configurations'].keys()
+            inst_id = list(inst_id)
+            inst_id = inst_id[0]
+            d['pulse energy'] = \
+                meta['instrument_configurations'][inst_id]['components'][0]['pulse energy']
+        except BaseException:
+            pass
+
+        try:
+            inst_id = meta['instrument_configurations'].keys()
+            inst_id = list(inst_id)
+            inst_id = inst_id[0]
+            d['pulse duration'] = \
+                meta['instrument_configurations'][inst_id]['components'][0]['pulse duration']
+        except BaseException:
+            pass
+
+        return d
